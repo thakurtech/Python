d909761bc9e82947c1c4e5f17b333445cbc20812
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2024-05-09 12:18:43 +0530

    Improved code readability and quality

--- Rockbnd .py
+++ Rockbnd .py
@@ -1,9 +1,23 @@
 # Rock Band Name Generator build by sumit Thakur
 
-print("Hi, I am your Bot and I'll help you in generating your Rockband name \n press Enter answer me some question and I'll generate that for you")
-name = input("Please Enter your Name - ")
-song = input("Great Name ,so what's your favorite song " +name + " ? " )
-animal = input("yeah that's an amazing song ,so what's your favorite animal " + name + " ? " )
-print("Thanks for these inputs " + name + "press any key to generate your band name")
-print("your Band Name is generated ,press any key to view ")
-print(name + song[0:5] + animal[0:5] )
+print("Hi, I am your Bot and I'll help you in generating your Rockband name \n press Enter and answer me some question and I'll generate that for you")
+name = None 
+song = None
+animal = None
+name = input("Please Enter your Name:")
+if name:
+    song = input("Great Name ,so what's your favorite song " +name + " ? " )
+else:
+    print("Name can't be empty.")
+if song:
+    animal = input(f"yeah that's an amazing song ,so what's your favorite animal {name}?")
+else:
+    print("Song can't be empty.")
+print("Thanks for these inputs " + name + "press Enter to generate your band name.")
+input()
+print("your Band Name is generated ,press Enter to view.")
+input()
+if len(song) >= 5 and len(animal) >= 5:
+    print(name + song[0:5] + animal[0:5] )
+else:
+    print("Your entered song or animal name is too short to generate!")
